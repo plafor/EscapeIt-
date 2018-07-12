469ea16faf799485ad7bb4212a57fbee233fd86c
Pierre Laforcade <pierre.laforcade@univ-lemans.fr>
2018-07-12 16:47:51 +0200

    bug corriges

---
+++ Assets/Scripts/Scripts for Menu/LoadSceneWithListScenarios.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneWithListScenarios : MonoBehaviour {
+
+	public void LoadSceneWithScenarios() {
+		StartCoroutine(LoadScreen());
+
+	}
+
+	IEnumerator LoadScreen()
+	{
+		yield return true; //new WaitForSeconds(1.0f); // wait time
+		SceneManager.LoadScene("ScenariosChoice");
+	}
+}
